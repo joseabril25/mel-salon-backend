92eb2c74a557eb3b7055697c55d836f573548b09
Se <jose.abril4@gmail.com>
2021-03-01 12:26:00 +0800

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a261afc28a37cf9798b6ed46639b9928'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3d507d49-d6b8-4bbe-950b-51b7ac2fa483'
+};
